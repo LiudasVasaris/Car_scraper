2a2fbe61f92a00ddb6006b677131f2b87b9d6ad6
LiudasVasaris <julius.cereska@gmail.com>
2022-12-03 19:00:15 +0200

    shell scripts for uploading packages to s3

---
+++ ETL/lambda_layer_creation.sh
@@ -0,0 +1,24 @@
+bucket_name="s3://car-scraper-vu-bucket/"
+package_name="car-scraper-package-etl.zip"
+
+mkdir packages
+cd packages || exit
+
+python3 -m venv venv
+source venv/bin/activate
+
+mkdir python
+cd python || exit
+
+pip install bs4 requests -t .
+
+rm -rf ./*dist-info
+
+cd ..
+
+zip -r $package_name python
+
+aws s3 cp $package_name $bucket_name
+
+cd ..
+rm -rf packages
