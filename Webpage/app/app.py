649c01cc4ac0814e3ca8770959ae7b4f06df40ba
LiudasVasaris <julius.cereska@gmail.com>
2022-12-07 22:21:12 +0200

    Working ap with css styling

--- Webpage/app/app.py
+++ Webpage/app/app.py
@@ -1,17 +1,21 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from dash_app_graph import create_dash_app_graph
-from utilities import get_landing_page, get_dataframe_data
+from utilities import get_dataframe_data
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def landing_page():
-    return get_landing_page()
+async def landing_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/table", response_class=HTMLResponse)
